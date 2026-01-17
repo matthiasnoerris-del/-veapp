4e530815bf60b712b55a83bdfd5120d38ba34172
matthiasnoerris-del <matthiasnoerris@hotmail.com>
2026-01-17 03:43:47 +0100

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,8 +1,9 @@
 const CACHE_NAME="oeveapp-cache-v2";
-const urlsToCache=["./","./index.html","./manifest.json","./icon-192.png","./icon-512.png","https://cdn.jsdelivr.net/npm/chart.js","https://fonts.googleapis.com/css2?family=Inter:wght@400;500;700&display=swap"];
+const urlsToCache=["./","./index.html","./manifest.json","./icon-192.png","./icon-511.png","https://cdn.jsdelivr.net/npm/chart.js","https://fonts.googleapis.com/css2?family=Inter:wght@400;500;700&display=swap"];
 
 self.addEventListener("install",event=>{event.waitUntil(caches.open(CACHE_NAME).then(cache=>cache.addAll(urlsToCache)));});
 self.addEventListener("fetch",event=>{event.respondWith(caches.match(event.request).then(response=>response||fetch(event.request)));});
+
 
 
 
